10899600144e13d1a89532d0de56154f918632fb
Mr.R <RakshitMeshram@users.noreply.github.com>
2018-04-03 11:35:05 +0530

    Create InsertionSort.c

---
+++ InsertionSort.c
@@ -0,0 +1,56 @@
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+ 
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = arr[i];
+       j = i-1;
+ 
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+   }
+}
+ 
+
+void printArray(int arr[], int n)
+{
+   int i;
+   for (i=0; i < n; i++)
+       printf("%d ", arr[i]);
+   printf("\n");
+}
+ 
+ 
+ 
+
+int main()
+{
+    
+    int n ,i;
+    printf("Enter the size of array : \t");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        arr[i]=rand()%1000;
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    insertionSort(arr, n);
+    printArray(arr, n);
+ 
+    return 0;
+}
